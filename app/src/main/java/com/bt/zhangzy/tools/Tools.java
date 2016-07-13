7bda99f5574d1530ff9b1aa084d32fccf5b5ffc7
zyoo5 <wxzya05@163.com>
2016-07-13 10:39:10 +0800

    手机号判断 增加17号码段

--- app/src/main/java/com/bt/zhangzy/tools/Tools.java
+++ app/src/main/java/com/bt/zhangzy/tools/Tools.java
@@ -65,7 +65,9 @@ public final class Tools {
      * @return
      */
     public static boolean IsPhoneNum(String mobiles) {
-        Pattern p = Pattern.compile("^((13[0-9])|(15[^4,\\D])|(18[0,5-9]))\\d{8}$");
+//        Pattern p = Pattern.compile("^((13[0-9])|(15[^4,\\D])|(18[0,5-9]))\\d{8}$");
+        Pattern p = Pattern.compile("^1[3|4|5|7|8]\\d{9}$");
+
         Matcher m = p.matcher(mobiles);
         return m.matches();
     }
@@ -309,7 +311,6 @@ public final class Tools {
 
         return distanceStr;
     }
-
 
 
 }
