63f593e99550837d9a30287c645ff8ba66453e1d
zyoo5 <wxzya05@163.com>
2016-08-06 17:04:09 +0800

    Bugly添加，用于收集和上传崩溃信息；

---
+++ bugly/src/main/java/params/devices/zhangzy/bugly/BuglySDK.java
@@ -0,0 +1,43 @@
+package params.devices.zhangzy.bugly;
+
+import android.content.Context;
+
+import com.tencent.bugly.crashreport.CrashReport;
+
+/**
+ * Created by ZhangZy on 2016-8-6.
+ */
+public class BuglySDK {
+
+    /*AppID：
+6d69bb7789
+AppKey：
+eafa11cb-f9c6-405d-84be-5758fde8662e
+*/
+    public static final String APP_ID = "6d69bb7789";
+    public static final String APP_KEY = "eafa11cb-f9c6-405d-84be-5758fde8662e";
+
+    static BuglySDK instance = new BuglySDK();
+
+    private BuglySDK() {
+    }
+
+    public static BuglySDK getInstance() {
+        return instance;
+    }
+
+    /**
+     *  初始化
+     * @param appContext
+     */
+    public void init(Context appContext) {
+        CrashReport.initCrashReport(appContext, APP_ID, false);
+    }
+
+    /**
+     * 测试
+     */
+    public void test() {
+        CrashReport.testJavaCrash();
+    }
+}
